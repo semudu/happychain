6d93284e3d1f7f017609b6ec6173e7ce9cf094ae
Serhat Durmaz <serhat.md@gmail.com>
2020-02-02 16:09:52 +0300

    procfile updated

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,16 @@
-from run import start_app
+import threading
 
-app = start_app()
+import app
+from app.job import Job
+from config import Config
+
+
+def start_app():
+    config = Config()
+    jobs = Job(config)
+
+    job_thread = threading.Thread(name="job_thread", target=jobs.send_periodically_messages, args=())
+    job_thread.setDaemon(True)
+    job_thread.start()
+
+    app.run(config)
