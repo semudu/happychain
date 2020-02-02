6c114f494c561936df89424d12ce760da28d97d4
Serhat Durmaz <serhat.md@gmail.com>
2020-02-02 16:29:05 +0300

    updated port

--- settings.py
+++ settings.py
@@ -6,8 +6,8 @@ class Settings:
     LOG_LEVEL = "debug"
     LOG_FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     APP_NAME = 'HappyChain'
-    HOST = '0.0.0.0'
-    PORT = '80'
+    HOST = '127.0.0.1'
+    PORT = '5000'
     DB_HOST = os.environ.get('DB_HOST')
     DB_PORT = os.environ.get('DB_PORT')
     DB_NAME = os.environ.get('DB_NAME')
