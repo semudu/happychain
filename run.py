907eb2303ce9552e66bd140cf7d7ee734435c185
Serhat Durmaz <serhat.md@gmail.com>
2020-02-07 22:10:03 +0300

    run.py updated

--- run.py
+++ run.py
@@ -3,11 +3,17 @@ from app.schedule import Schedule
 
 
 def start_app():
-    scheduled_jobs = Schedule()
-    flask_app = App()
+    try:
+        scheduled_jobs = Schedule()
+        flask_app = App()
+        scheduled_jobs.setDaemon(True)
+        flask_app.setDaemon(True)
 
-    scheduled_jobs.start()
-    flask_app.start()
+        scheduled_jobs.start()
+        flask_app.start()
 
-    scheduled_jobs.join()
-    flask_app.join()
+        scheduled_jobs.join()
+        flask_app.join()
+
+    except (KeyboardInterrupt, SystemExit):
+        print("Threads finished.")
